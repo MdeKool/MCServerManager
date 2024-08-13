111fe144db1558b2ff5f3a82904e685c721f8b51
MdeKool <matthijsdekool@gmail.com>
2024-08-13 21:30:45 +0200

    Changed if-check for existence of dir to exists_ok argument in os.makedirs

--- utils/files.py
+++ utils/files.py
@@ -6,8 +6,7 @@ from zipfile import ZipFile
 def make_dir(dir_name):
     base_dir = "/home/servers/"
     save_dir = os.path.join(base_dir, dir_name)
-    if not os.path.isdir(save_dir):
-        os.makedirs(save_dir)
+    os.makedirs(save_dir, exist_ok=True)
     return save_dir
 
 
