58d5f598182d2f91cf21e5dfbe1c1a1f8dd617da
M de Kool <matthijsdekool@gmail.com>
2024-08-13 16:13:19 +0200

    Debug prints

--- routers/upload.py
+++ routers/upload.py
@@ -9,8 +9,10 @@ router = APIRouter(prefix="/upload")
 async def file_upload(file: UploadFile = File(...)):
     util.make_dir("~/.temp/missing_mods")
     contents = await file.read()
+    print(contents)
     with open(file.filename, "wb+") as f:
         f.write(contents)
+    print("Write success")
     return {
         "filename": file.filename
     }
