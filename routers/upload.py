2bee2f9901e720356cdf47056dfbb7994e71ba1f
M de Kool <matthijsdekool@gmail.com>
2024-08-13 07:53:03 +0200

    Testing file upload

--- routers/upload.py
+++ routers/upload.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, File
 
 router = APIRouter(prefix="/upload")
 
 
 @router.post("/")
 async def file_upload(request: Request):
-    print("Got a new file:", await request.body())
+    print("Got a new file:", File(await request.body()))
