3a0e332b5ec56dfa857a593040588de4898dd3d0
M de Kool <matthijsdekool@gmail.com>
2024-08-13 15:12:35 +0200

    Change parameters back to include request param

--- routers/upload.py
+++ routers/upload.py
@@ -4,6 +4,7 @@ router = APIRouter(prefix="/upload")
 
 
 @router.post("/")
-async def file_upload(file: UploadFile = File(...)):
+async def file_upload(request: Request, file: UploadFile = File(...)):
+    print(request.form())
     contents = await file.read()
     print("Got a new file:", contents)
